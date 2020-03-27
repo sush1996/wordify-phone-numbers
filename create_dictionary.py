ab92e7e3ec81177891c0724539a09e69c373e2f4
sush1996 <sushruth96@gmail.com>
2020-03-27 16:10:23 -0400

    updated code to include for sorting the words

--- create_dictionary.py
+++ create_dictionary.py
@@ -1,7 +1,7 @@
 '''
 To create a final list of words from all text files in the folder, the following steps are undertaken:
 
-1. Choose words with length less than or equal to 7
+1. Choose words with length less than or equal to 7 and greater than 1
 2. Exclude words which are not alphabetical in nature
 3. Convert all the letters of the chosen word to uppercase
 
@@ -22,22 +22,20 @@ for file_name in os.listdir(filepath):
     for word in file_data:
         word = word.strip('\n')
 
-        if len(word) <= 7 and word.isalpha() == True:   #Criteria 1 and 2 in the description
+        if len(word) <= 7 and len(word) > 1 and word.isalpha() == True:   #Criteria 1 and 2 in the description
             word = word.upper()
             all_words.append(word)
         else:
             continue
 
 all_words = list(set(all_words))
+all_words = sorted(all_words, key=lambda word_len:len(word_len))
 
 with open("final_dictionary.txt", "w+") as f:
     for word in all_words:
         f.write(word+'\n')
 
-
 '''
 Input: Takes in folder containing all the text files to pre-process and compile
 Output: final_dictionary.txt - final version containing a set of all the words in all the text files that were compiled
 '''
-
-    
