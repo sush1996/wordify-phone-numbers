eed6ba1c8ebe71e53729d3919e4f42ed9e41d251
sush1996 <sushruth96@gmail.com>
2020-03-27 07:07:11 -0400

    removed/edited a few comments

--- all_wordifications.py
+++ all_wordifications.py
@@ -11,23 +11,23 @@ US phone number formats taken into consideration:
 '''
 
 import itertools
-from fix_hyphenation import fix_hyphenation_all
+from fix_hyphenation import *
 
-#number = "1-800-724-6837"
+# number = "1-800-724-6837"
 
-def all_wordifications(number):  #type(number) : string
+def all_wordifications(number):  # type(number) : string
 
-    #Dictionary to relate numbers with its associated letters (referred to mobile phone keypad)
-    #US phone numbers can only start from a number between 2-9 (excluding +1 part of the number)
+    # dictionary to relate numbers with its associated letters (referred to mobile phone keypad)
+    # US phone numbers can only start from a number between 2-9 (excluding +1 part of the number)
     num_word_dict = {'2': ['A','B','C'], '3':['D','E','F'], '4':['G','H','I'], '5':['J','K','L'],
                      '6':['M','N','O'], '7':['P','Q','R','S'], '8':['T','U','V'], '9':['W','X','Y','Z'], '0':['0']}
 
-    #Account for different styles of writing a phone number
-    #Retain format for the unwordified part of the the number
+    # account for different styles of writing a phone number
+    # retain format for the unwordified part of the the number
 
     if '-' not in number:
         
-        #Accounts for all number formats in 0.
+        # accounts for all number formats in 0.
         if number[0] == '1':
             start_ind = 4
         elif number[0] == '+':
@@ -41,7 +41,7 @@ def all_wordifications(number):  #type(number) : string
     else:
         number_list = number.split('-')
 
-        #Accounts for all number formats in 1.
+        # accounts for all number formats in 1.
         if len(number_list) == 2:
             if len(number_list[0]) <= 2:                                                  
                 unwordified_num = number_list[0] + "-" + number_list[1][:3]
@@ -50,12 +50,12 @@ def all_wordifications(number):  #type(number) : string
                 unwordified_num = number_list[0] + "-"
                 num_to_wordify = number_list[1]
 
-        #Accounts for all number formats in 2.
+        # accounts for all number formats in 2.
         elif len(number_list) == 3:
             unwordified_num = number_list[0] + "-"
             num_to_wordify = number_list[1] + number_list[2]
         
-        #Accounts for all number formats in 3.
+        # accounts for all number formats in 3.
         elif len(number_list) == 4:                                                 
             unwordified_num = number_list[0] + "-" + number_list[1] + "-"
             num_to_wordify = number_list[2] + number_list[3]
@@ -63,19 +63,20 @@ def all_wordifications(number):  #type(number) : string
     nums_letters_list = []
 
     for num in num_to_wordify:
-        nums_letters_list.append([num] + num_word_dict[num])  #Create the set of input lists to be used for itertools.product
+        nums_letters_list.append([num] + num_word_dict[num])  # create the set of input lists to be used for itertools.product
 
-    wordified_nums_lists = list(itertools.product(*nums_letters_list)) #itertools.product creates a cartesian product of all the input lists
+    wordified_nums_lists = list(itertools.product(*nums_letters_list)) # itertools.product creates a cartesian product of all the input lists
 
     all_wordified_numbers = []
 
+    #all combinations
     for word_list in wordified_nums_lists:
         wordfied_num = "".join(char for char in word_list)
-        temp_var = unwordified_num + fix_hyphenation_all(wordfied_num)   #Appending the un-wordified part of the phone number to the wordified part
+        temp_var = unwordified_num + fix_hyphenation_all(wordfied_num)   # appending the un-wordified part of the phone number to the wordified part
         all_wordified_numbers.append(temp_var)
 
-    return all_wordified_numbers
-    
+    return all_wordified_numbers   # type(all_wordified_nmbers) : list of strings
+   
 '''
 Input: 1-800-724-6837
 
@@ -95,15 +96,4 @@ Sample results:
 Number of combinations: 25600
 
 Time taken: 0.05711
-'''
-
-'''
-Completed
-1. Take care of hyphenating the results
-2. Find publicly available english words dictionary to create only useful/desireable wordified-numbers 
-
-Next Steps: 
-3. Filter results of all_wordifications based on a dictionary 
-4. Design hyphenation for the filtered results 
-5. Consider designing test cases to validate results
 '''
