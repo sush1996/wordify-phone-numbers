fe4d832e17165dbd51f9c027d3413765079fd667
sush1996 <sushruth96@gmail.com>
2020-03-27 16:04:06 -0400

    updated code for efficiency

--- filtered_wordifications.py
+++ filtered_wordifications.py
@@ -1,9 +1,15 @@
 '''
 Results from all_wordifications are filtered down to meaningful words by making use of a dictionary created using create_dictionary
 Same code structure as all_wordifications.py but incorporates a dictionary to only take dictionary words into account.
+
+Results are highly dependent on the dictionary that is used to filter the words.
+I've chosen to incorporate the biggest dictionary
+
+Current content in 'dictionaries' folder
+1. 20k.txt :    https://github.com/first20hours/google-10000-english/blob/master/20k.txt
+2. words.txt :  https://github.com/dwyl/english-words/blob/master/words.txt    
 '''
 
-import time
 import itertools
 from utils import *
 
@@ -56,39 +62,37 @@ def filtered_wordifications(number, dictionary): # type(number) : string, type(d
     wordified_nums_lists = list(itertools.product(*nums_letters_list))  # itertools.product creates a cartesian product of all the input lists
 
     filtered_wordified_numbers = []
-    start = time.time()
     
     # filtered Combinations
     dictionary = open(dictionary).readlines()
     dictionary = list(map(lambda line: line.strip('\n'), dictionary))
+    dict_of_words = {word for word in dictionary}	# put the list of words in a hashmap, allows for much faster querying
 
     wordified_nums_lists = wordified_nums_lists[1:]     # the number itself will be included since it's a valid combination, but we don't need it
-
+	
     for word_list in wordified_nums_lists:
-        wordfied_num = "".join(char for char in word_list)
-        list_of_subwords = extract_string(wordfied_num)     # extract_string fetches the sub-words available in an alphanumerical string
+        word_string = "".join(char for char in word_list)
+        list_of_subwords = extract_strings(word_string)     # extract_string fetches the sub-words available in an alphanumerical string
 
         flag = 0
         
         # if the wordfified number has multiple sub-words, then we consider the number if and only if all the sub-words are a part of the dictionary
         for sub_word in list_of_subwords:
-            if sub_word not in dictionary:		
+            if sub_word not in dict_of_words:		
                 flag = 1
                 break
 
         if flag == 0:
-            temp_var = unwordified_num + fix_hyphenation_all(wordfied_num)
+            temp_var = unwordified_num + fix_hyphenation(word_string)
             filtered_wordified_numbers.append(temp_var)
 
-    # we sort the results in the order of increasing number of '-', this would put the longer wordified numbers in front of the list
-    filtered_wordified_numbers = sorted(filtered_wordified_numbers, key=lambda number: number.count('-')) 
+    # sort the results by the number of alphabets in the wordified number
+    # puts better results at the front of the list than results yielded by sorting by the count of '-' 
+    filtered_wordified_numbers = sorted(filtered_wordified_numbers, key=count_alphabets, reverse=True) 
+
     end = time.time()
 
-    print(end-start)
-    print(len(filtered_wordified_numbers))
     return filtered_wordified_numbers      # type(all_wordified_numbers) : list of strings
-
-print(filtered_wordifications('1-800-724-6837', '20k_final_dictionary.txt'))
 
 '''
 Input: 1-800-724-6837
@@ -97,18 +101,27 @@ Output:
 
 Sample results:
 
-['1-800-PAINTER', '1-800-724-OVER', '1-800-72-INTER', '1-800-PAIN-837', '1-800-PAINT-37', '1-800-PCG-6837', '1-800-PCI-6837',
- '1-800-RAG-6837', '1-800-RAI-6837', '1-800-RAIN-837', '1-800-RBI-6837', '1-800-SAINT-37', '1-800-SBIN-837', '1-800-SCH-6837',
- '1-800-SCI-6837', '1-800-724-68-DP', '1-800-724-68-DQ', '1-800-724-68-DR', '1-800-724-68-DS', '1-800-724-68-EP', '1-800-724-68-EQ',
- '1-800-724-68-ER', '1-800-724-68-ES', '1-800-724-68-FP', '1-800-724-68-FR', '1-800-724-68-FS', '1-800-724-6-TDS', '1-800-724-6-TER',
- '1-800-724-6-TES', '1-800-724-6-UDP', '1-800-724-6-VER', '1-800-724-MT-37', '1-800-724-MU-37', '1-800-724-MUD-7', '1-800-724-MV-37',
- '1-800-724-NT-37', '1-800-724-NU-37', '1-800-724-NV-37', '1-800-724-OT-37', '1-800-724-OU-37', '1-800-724-OV-37', '1-800-72-GM-837',
- '1-800-72-GMT-37', '1-800-72-GN-837', '1-800-72-GNU-37', '1-800-72-GO-837', '1-800-72-GOT-37', '1-800-72-GOV-37', '1-800-72-HM-837', 
- '1-800-72-HN-837', '1-800-72-HO-837', '1-800-72-HOT-37', '1-800-72-IM-837', '1-800-72-IN-837', '1-800-72-INT-37', '1-800-72-IO-837', ...]
+['1-800-PAINTER', '1-800-7-CINTER', '1-800-PA-4-MUDS', '1-800-PA-4-OTDR', '1-800-PA-4-OTES', '1-800-PA-4-OUDS', '1-800-PA-4-OVER',
+ '1-800-PAG-6-TDR', '1-800-PAG-6-TDS', '1-800-PAG-6-TER', '1-800-PAG-6-TES', '1-800-PAG-6-TFP', '1-800-PAG-6-TFR', '1-800-PAG-6-TFS',
+ '1-800-PAG-6-UDP', '1-800-PAG-6-UDR', '1-800-PAG-6-UDS', '1-800-PAG-6-UFS', '1-800-PAG-6-VER', '1-800-PAG-6-VFR', '1-800-PAG-6-VFS',
+ '1-800-PAH-6-TDR', '1-800-PAH-6-TDS', '1-800-PAH-6-TER', '1-800-PAH-6-TES', '1-800-PAH-6-TFP', '1-800-PAH-6-TFR', '1-800-PAH-6-TFS', 
+ '1-800-PAH-6-UDP', '1-800-PAH-6-UDR', '1-800-PAH-6-UDS', '1-800-PAH-6-UFS', '1-800-PAH-6-VER', '1-800-PAH-6-VFR', '1-800-PAH-6-VFS', 
+ '1-800-PAHO-8-DP', '1-800-PAHO-8-DQ', '1-800-PAHO-8-DR', '1-800-PAHO-8-DS', '1-800-PAHO-8-EP', '1-800-PAHO-8-EQ', '1-800-PAHO-8-ER', 
+ '1-800-PAHO-8-ES', '1-800-PAHO-8-FP', '1-800-PAHO-8-FR', '1-800-PAHO-8-FS', '1-800-PAIN-8-DP', '1-800-PAIN-8-DQ', '1-800-PAIN-8-DR', 
+ '1-800-PAIN-8-DS', '1-800-PAIN-8-EP', '1-800-PAIN-8-EQ', '1-800-PAIN-8-ER', '1-800-PAIN-8-ES', '1-800-PAIN-8-FP', '1-800-PAIN-8-FR', 
+ '1-800-PAIN-8-FS', '1-800-PB-4-MUDS', '1-800-PB-4-OTDR', '1-800-PB-4-OTES', '1-800-PB-4-OUDS', '1-800-PB-4-OVER', '1-800-PC-4-MUDS', 
+ '1-800-PC-4-OTDR', '1-800-PC-4-OTES', '1-800-PC-4-OUDS', '1-800-PC-4-OVER', '1-800-PCG-6-TDR', '1-800-PCG-6-TDS', '1-800-PCG-6-TER', 
+ '1-800-PCG-6-TES', '1-800-PCG-6-TFP', '1-800-PCG-6-TFR', '1-800-PCG-6-TFS', '1-800-PCG-6-UDP', '1-800-PCG-6-UDR', '1-800-PCG-6-UDS', 
+ '1-800-PCG-6-UFS', '1-800-PCG-6-VER', '1-800-PCG-6-VFR', '1-800-PCG-6-VFS', '1-800-PCH-6-TDR', '1-800-PCH-6-TDS', '1-800-PCH-6-TER', 
+ '1-800-PCH-6-TES', '1-800-PCH-6-TFP', '1-800-PCH-6-TFR', '1-800-PCH-6-TFS', '1-800-PCH-6-UDP', '1-800-PCH-6-UDR', '1-800-PCH-6-UDS', 
+ '1-800-PCH-6-UFS', '1-800-PCH-6-VER', '1-800-PCH-6-VFR', '1-800-PCH-6-VFS', '1-800-PCI-6-TDR', '1-800-PCI-6-TDS', '1-800-PCI-6-TER', 
+ '1-800-PCI-6-TES', '1-800-PCI-6-TFP', '1-800-PCI-6-TFR', '1-800-PCI-6-TFS', '1-800-PCI-6-UDP', '1-800-PCI-6-UDR', '1-800-PCI-6-UDS', 
+ '1-800-PCI-6-UFS', '1-800-PCI-6-VER', '1-800-PCI-6-VFR', '1-800-PCI-6-VFS', '1-800-QA-4-MUDS', '1-800-QA-4-OTDR', '1-800-QA-4-OTES', 
+ '1-800-QA-4-OUDS', '1-800-QA-4-OVER', '1-800-QB-4-MUDS', '1-800-QB-4-OTDR', '1-800-QB-4-OTES', '1-800-QB-4-OUDS', '1-800-QB-4-OVER', ... ]
 
-Number of combinations: 1093
+Number of combinations: 2119
 
-Time taken: 2.95
+Time taken: 0.075
 '''
 
 '''
@@ -117,8 +130,9 @@ Completed
 2. Find publicly available english words dictionary to create only useful/desireable wordified-numbers 
 3. Filter results of all_wordifications based on a dictionary 
 4. Design hyphenation for the filtered results (was not needed)
+6. "Make code more efficient, very slow!" ~ roughly 50 times faster now (used a hashmap instead of a list to store the dictionary)
 
 Next Steps: 
 5. Consider designing test cases to validate results
-6. Make code more efficient, very slow!
+
 '''
