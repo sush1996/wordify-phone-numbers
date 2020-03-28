8475235334cc474e801c6a71b0b171771ba9c655
sush1996 <sushruth96@gmail.com>
2020-03-28 05:46:48 -0400

    refactored code to work as a function

--- create_dictionary.py
+++ create_dictionary.py
@@ -1,4 +1,8 @@
 '''
+Function : create_dictionary()
+
+Function Design: 
+
 To create a final list of words from all text files in the folder, the following steps are undertaken:
 
 1. Choose words with length less than or equal to 7 and greater than 1
@@ -6,34 +10,40 @@ To create a final list of words from all text files in the folder, the following
 3. Convert all the letters of the chosen word to uppercase
 
 Current content in 'dictionaries' folder
-1. 20k.txt :    https://github.com/first20hours/google-10000-english/blob/master/20k.txt
-2. words.txt :  https://github.com/dwyl/english-words/blob/master/words.txt    
+
+i. 20k.txt :    https://github.com/first20hours/google-10000-english/blob/master/20k.txt
+ii. words.txt :  https://github.com/dwyl/english-words/blob/master/words.txt    
 '''
 
 import os
 
-filepath = 'dictionaries/'  #Make sure to include any relevant text files in the 'dictionaries' folder
-all_words = []
+# input_path: dictionaries/
+# output_path: final_dictionary.txt
 
-for file_name in os.listdir(filepath):
-    file_to_read = open(filepath+"/"+file_name, 'r+')
-    file_data = file_to_read.readlines()
+def create_dictionary(input_path, output_path):  # ensure input_path leads to a directory and output_path leads to a .txt file
     
-    for word in file_data:
-        word = word.strip('\n')
+    filepath = input_path  # make sure to include any relevant text files in this directory
+    all_words = []
 
-        if len(word) <= 7 and len(word) > 1 and word.isalpha() == True:   #Criteria 1 and 2 in the description
-            word = word.upper()
-            all_words.append(word)
-        else:
-            continue
+    for file_name in os.listdir(filepath):
+        file_to_read = open(filepath + "/" + file_name, 'r+')
+        file_data = file_to_read.readlines()
+        
+        for word in file_data:
+            word = word.strip('\n')
 
-all_words = list(set(all_words))
-all_words = sorted(all_words, key=lambda word_len:len(word_len))
+            if len(word) <= 7 and len(word) > 1 and word.isalpha() == True:    # criteria 1 and 2 in the description
+                word = word.upper()
+                all_words.append(word)
+            else:
+                continue
 
-with open("final_dictionary.txt", "w+") as f:
-    for word in all_words:
-        f.write(word+'\n')
+    all_words = list(set(all_words))
+    all_words = sorted(all_words, key=lambda word_len: len(word_len))
+
+    with open(output_path, "w+") as f:
+        for word in all_words:
+            f.write(word+'\n')
 
 '''
 Input: Takes in folder containing all the text files to pre-process and compile
